6a65ad05b0b0219046061cd9b68e6418140fcbb3
David Wu <david204code@gmail.com>
2020-12-04 18:16:42 +0000

    Sample component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ import ReactDom from 'react-dom';
 
 
 function Greeting(){
-  return <h4>This is David, my first component</h4>;
+  return (
+    <div>
+      <Person />
+      <Message />
+      <Message1 />
+    </div>
+  );
 }
+
+const Person = () => <h2>David Wu</h2>;
+const Message = () => {
+  return <h4>Edit This is David, my first component</h4>
+}
+const Message1 = () => <h5>Hello David</h5>
 
 ReactDom.render(<Greeting/>, document.getElementById('root'));
