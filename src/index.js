9f898a3c6635830af5a89e658d7997623fa92e79
David Wu <david204code@gmail.com>
2020-12-10 18:50:57 +0000

    Passing in props for dynamic rendering

--- src/index.js
+++ src/index.js
@@ -3,25 +3,35 @@ import ReactDom from 'react-dom';
 import './index.css';
 
 // set up the variables
-const author = 'Naomi Zacharias'
-const title = 'Little Prince, Little Prince: What Will You Be?'
-const img = 'https://m.media-amazon.com/images/I/61jcNGAvh7L._AC_UY218_.jpg'
 
+const firstBook = {
+  img: 'https://m.media-amazon.com/images/I/61jcNGAvh7L._AC_UY218_.jpg',
+  title: 'Little Prince, Little Prince: What Will You Be?',
+  author: 'Naomi Zacharias',
+}
+
+const secondBook = {
+  img: 'https://m.media-amazon.com/images/I/61JunrhQrAL._AC_UY218_.jpg',
+  title: 'Grand Weaver The PB: How God Shapes Us Through the Events of Our Lives',
+  author: 'Ravi Zacharias',
+}
 
 function BookList(){
   return (
     <section className ="booklist">
-      <Book />
+      <Book img ={firstBook.img} title ={firstBook.title} author ={firstBook.author}/>
+      <Book img ={secondBook.img} title ={secondBook.title} author ={secondBook.author}/>
     </section>
   );
 }
 // Explicit return with the retun keyword
-const Book = () => {
+const Book = (props) => {
+  console.log(props)
   return (
     <article className ='book'>
-      <img src ={img} alt = ""/>
-      <h1>{title}</h1>
-      <h4 style ={{color :"#617d98", fontSize: '0.75rem', marginTop: '0.25rem'}}>{author.toLocaleUpperCase()}</h4>
+      <img src ={props.img} alt = ""/>
+      <h1>{props.title}</h1>
+      <h4 style ={{color :"#617d98", fontSize: '0.75rem', marginTop: '0.25rem'}}>{props.author.toLocaleUpperCase()}</h4>
     </article>
   )
 }
