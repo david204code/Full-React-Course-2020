83889370e6bfdfef63b38751fcd0da3af7bd570b
David Wu <david204code@gmail.com>
2020-12-10 18:10:15 +0000

    set up the variables to pass in for render

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
+// set up the variables
+const author = 'Naomi Zacharias'
+const title = 'Little Prince, Little Prince: What Will You Be?'
+const img = 'https://m.media-amazon.com/images/I/61jcNGAvh7L._AC_UY218_.jpg'
+
 
 function BookList(){
   return (
@@ -10,13 +15,11 @@ function BookList(){
     </section>
   );
 }
-const author = 'Naomi Zacharias'
 // Explicit return with the retun keyword
 const Book = () => {
-  const title = 'Little Prince, Little Prince: What Will You Be?'
   return (
     <article className ='book'>
-      <img src ="https://m.media-amazon.com/images/I/61jcNGAvh7L._AC_UY218_.jpg" alt = ""/>
+      <img src ={img} alt = ""/>
       <h1>{title}</h1>
       <h4 style ={{color :"#617d98", fontSize: '0.75rem', marginTop: '0.25rem'}}>{author.toLocaleUpperCase()}</h4>
     </article>
