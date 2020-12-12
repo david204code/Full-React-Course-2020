ec3b3c6765dbbac52eb7f336d47f7776fb500915
David Wu <david204code@gmail.com>
2020-12-12 20:26:23 +0000

    mapping over a simple list

--- src/index.js
+++ src/index.js
@@ -3,35 +3,60 @@ import ReactDom from 'react-dom';
 import './index.css';
 
 // set up the variables
+// const firstBook = {
+//   img: 'https://m.media-amazon.com/images/I/61jcNGAvh7L._AC_UY218_.jpg',
+//   title: 'Little Prince, Little Prince: What Will You Be?',
+//   author: 'Naomi Zacharias',
+// }
 
-const firstBook = {
-  img: 'https://m.media-amazon.com/images/I/61jcNGAvh7L._AC_UY218_.jpg',
-  title: 'Little Prince, Little Prince: What Will You Be?',
-  author: 'Naomi Zacharias',
-}
+// const secondBook = {
+//   img: 'https://m.media-amazon.com/images/I/61JunrhQrAL._AC_UY218_.jpg',
+//   title: 'Grand Weaver The PB: How God Shapes Us Through the Events of Our Lives',
+//   author: 'Ravi Zacharias',
+// }
 
-const secondBook = {
-  img: 'https://m.media-amazon.com/images/I/61JunrhQrAL._AC_UY218_.jpg',
-  title: 'Grand Weaver The PB: How God Shapes Us Through the Events of Our Lives',
-  author: 'Ravi Zacharias',
-}
+// set up array
+const books = [
+  
+  {
+    img: 'https://m.media-amazon.com/images/I/61jcNGAvh7L._AC_UY218_.jpg',
+    title: 'Little Prince, Little Prince: What Will You Be?',
+    author: 'Naomi Zacharias',
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/61JunrhQrAL._AC_UY218_.jpg',
+    title: 'Grand Weaver The PB: How God Shapes Us Through the Events of Our Lives',
+    author: 'Ravi Zacharias',
+  },
 
+];
+const names = ['john', 'peter', 'susan']
+// map method with parameter
+const newNames  = names.map((name) => {
+  // console.log(name);
+  return (
+    <h1>{name}</h1>
+  );
+});
+console.log(newNames);
 function BookList(){
   return (
     <section className ="booklist">
-      <Book 
+      {newNames}
+
+      {/* <Book 
         img ={firstBook.img} 
         title ={firstBook.title} 
         author ={firstBook.author}
-      >
+      > */}
         {/* child props */}
-        <p>Deserunt id reprehenderit exercitation laboris minim exercitation sint magna laborum. Ex commodo et commodo ut proident tempor laboris. Excepteur exercitation sint ad voluptate. Minim incididunt voluptate laboris ullamco magna enim id occaecat commodo deserunt mollit eu excepteur. Aliquip sunt aute Lorem reprehenderit laborum labore magna do irure culpa proident aute deserunt. Est velit dolore sit do duis ex dolor voluptate veniam.</p>
-      </Book>
-      <Book 
+        {/* <p>Deserunt id reprehenderit exercitation laboris minim exercitation sint magna laborum. Ex commodo et commodo ut proident tempor laboris. Excepteur exercitation sint ad voluptate. Minim incididunt voluptate laboris ullamco magna enim id occaecat commodo deserunt mollit eu excepteur. Aliquip sunt aute Lorem reprehenderit laborum labore magna do irure culpa proident aute deserunt. Est velit dolore sit do duis ex dolor voluptate veniam.</p> */}
+      {/* </Book> */}
+      {/* <Book 
         img ={secondBook.img} 
         title ={secondBook.title} 
         author ={secondBook.author}
-      />
+      /> */}
     </section>
   );
 }
@@ -48,7 +73,7 @@ const Book = (props) => {
       <img src ={props.img} alt = ""/>
       <h1>{props.title}</h1>
       <h4 style ={{color :"#617d98", fontSize: '0.75rem', marginTop: '0.25rem'}}>{props.author.toLocaleUpperCase()}</h4>
-      {props.children}
+      {/* {props.children} */}
     </article>
   )
 }
